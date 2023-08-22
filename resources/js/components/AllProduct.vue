f38926ff60762839a4d45447c09b23b6e6d96c0a
joseConejoMadureira <jose.games15@outlook.com>
2023-08-21 23:09:21 -0300

    list

--- resources/js/components/AllProduct.vue
+++ resources/js/components/AllProduct.vue
@@ -1,57 +1,62 @@
 <template>
-    <div>
-        <h2 class="text-center">Products List</h2>
+  <div>
+    <h2 class="text-center">Products List</h2>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Price</th>
-                <!-- <th>Actions</th> -->
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="product in products" :key="product.id">
-                <td>{{ product.id }}</td>
-                <td>{{ product.name }}</td>
-                <td>{{ product.price }}</td>
-                <td>
-                    <div class="btn-group" role="group">
-                        <router-link :to="{name: 'edit', params: { id: product.id }}" class="btn btn-success">Edit</router-link>
-                        <button class="btn btn-danger" @click="deleteProduct(product.id)">Delete</button>
-                    </div>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Price</th>
+          <!-- <th>Actions</th> -->
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" :key="product.id">
+            <td>{{ product.id }}</td>
+          <td>{{ product.name }}</td>
+          <td>{{ product.price }}</td>
+          <td>
+            <div class="btn-group" role="group">
+              <router-link
+                :to="{ name: 'edit', params: { id: product.id } }"
+                class="btn btn-success"
+                >Edit</router-link
+              >
+              <button class="btn btn-danger" @click="deleteProduct(product.id)">
+                Delete
+              </button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                products: [{"id":8,"name":"xbox 1","price":2500},{"id":9,"name":"xbox 2","price":2500},{"id":10,"name":"xbox 1","price":2500},{"id":11,"name":"xbox 2","price":2500}]
-            }
-        },
-        created() {
-            this.axios
-                .get('http://localhost:8000/api/products/')
-                .then(response => {
-                    this.products = response.data;
+import axios from "axios";
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8000/api/products/")
+      .then((response) => (this.products = response.data));
 
-                });
-        },
-        methods: {
-            deleteProduct(id) {
-                this.axios
-                    .delete(`http://localhost:8000/api/products/${id}`)
-                    .then(response => {
-                        let i = this.products.map(data => data.id).indexOf(id);
-                        this.products.splice(i, 1)
-                    });
-            }
-        }
-    }
+  },
+  methods: {
+    deleteProduct(id) {
+      this.axios
+        .delete(`http://localhost:8000/api/products/${id}`)
+        .then((response) => {
+          let i = this.products.map((data) => data.id).indexOf(id);
+          this.products.splice(i, 1);
+        });
+    },
+  },
+};
 </script>
