6a8ea53be030ba24730300800d86ac4f499d1d58
joseConejoMadureira <jose.games15@outlook.com>
2023-08-21 22:17:26 -0300

    front end

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AllProduct from '../components/AllProduct.vue';
+import CreateProduct from '../components/CreateProduct.vue';
+import EditProduct from '../components/EditProduct.vue';
 
 const routes = [
+    {
+        name: 'home',
+        path: '/',
+        component: AllProduct
+    },
+    {
+        name: 'create',
+        path: '/create',
+        component: CreateProduct
+    },
+    {
+        name: 'edit',
+        path: '/edit/:id',
+        component: EditProduct
+    }
 ]
 
 const router = createRouter({
