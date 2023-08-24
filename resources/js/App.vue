00f1e2b0b2433075c12ba079e9396f7bb1882f71
joseConejoMadureira <jose.games15@outlook.com>
2023-08-24 19:22:39 -0300

    identacao

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,18 +1,17 @@
 <template>
-    <nav-comp></nav-comp>
+  <nav-comp></nav-comp>
   <main>
     <section class="container">
       <router-view> </router-view>
     </section>
   </main>
-<footerComp> </footerComp>
-
+  <footerComp> </footerComp>
 </template>
 
 <script>
-import footerComp from './components/Footer.vue';
-import navComp from './components/Nav.vue'
+import footerComp from "./components/Footer.vue";
+import navComp from "./components/Nav.vue";
 export default {
-
-  components: { footerComp,navComp}};
+  components: { footerComp, navComp },
+};
 </script>
