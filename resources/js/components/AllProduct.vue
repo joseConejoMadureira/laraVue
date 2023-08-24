59f19788f911fc64087d1bd11cb6d1ca379de673
joseConejoMadureira <jose.games15@outlook.com>
2023-08-23 23:12:52 -0300

    table

--- resources/js/components/AllProduct.vue
+++ resources/js/components/AllProduct.vue
@@ -2,38 +2,43 @@
   <div>
     <h2 class="text-center">Products List</h2>
 
-    <table class="table">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Action</th>
-          <!-- <th>Actions</th> -->
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="product in products" :key="product.id">
-          <td>{{ product.id }}</td>
-          <td>{{ product.name }}</td>
-          <td>{{ product.price }}</td>
-          <td>
-            <div class="btn-group" role="group">
-              <router-link
-                :to="{ name: 'edit', params: { id: product.id } }"
-                class="btn btn-success"
-                >Edit</router-link
-              >
-              <button class="btn btn-danger" @click="deleteProduct(product.id)">
-                Delete
-              </button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="table-responsive-sm">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Action</th>
+            <!-- <th>Actions</th> -->
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <th scope="row">{{ product.id }}</th>
 
+            <td>{{ product.name }}</td>
+            <td>{{ product.price }}</td>
+            <td>
+              <div class="btn-group" role="group">
+                <router-link
+                  :to="{ name: 'edit', params: { id: product.id } }"
+                  class="btn btn-success"
+                  >Edit</router-link
+                >
+                <button
+                  class="btn btn-danger"
+                  @click="deleteProduct(product.id)"
+                >
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -48,7 +53,6 @@ export default {
     axios
       .get("http://localhost:8000/api/products/")
       .then((response) => (this.products = response.data));
-
   },
   methods: {
     deleteProduct(id) {
