602149e8d0be67fec98dd2f10a637730e8280328
joseConejoMadureira <jose.games15@outlook.com>
2023-08-24 18:57:42 -0300

    footer componente

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -22,61 +22,14 @@
       <router-view> </router-view>
     </section>
   </main>
-  <footer class="bg-secondary pt-4 pb-4">
-    <div class="container d-flex justify-content-between align-items-center">
-      <span> © 2023 Todos os direitos reservados </span>
+<footerComp> </footerComp>
 
-      <ul class="social">
-        <li>
-          <a
-            href="https://www.linkedin.com/in/jos%C3%A9-antonio-conejo-madureira-785989230/"
-            class="text-hide"
-            >Linkedin</a
-          >
-        </li>
-        <li>
-          <a href="https://github.com/joseConejoMadureira" class="text-hide"
-            >Github</a
-          >
-        </li>
-      </ul>
-    </div>
-  </footer>
 </template>
 
 <script>
-export default {};
+import footerComp from './components/Footer.vue';
+
+export default {
+
+  components: { footerComp }};
 </script>
-<style>
-.social a {
-  display: block;
-  width: 36px;
-  height: 36px;
-  text-indent: -9999px;
-}
-
-.social li {
-  display: inline-block;
-  margin-left: 1em;
-}
-.social a[href='https://www.linkedin.com/in/jos%C3%A9-antonio-conejo-madureira-785989230/']
-{
-  background: url(./assets/icone-linkedin.png);
-  background-size: 36px 36px;
-}
-
-.social a[href='https://github.com/joseConejoMadureira']
-{
-  background: url(./assets/icone-github.png);
-  background-size: 36px 36px;
-}
-footer {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: auto;
-  width: 100vw;
-  padding-top: 40px;
-}
-</style>
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+  <footer class="bg-secondary pt-4 pb-4">
+    <div class="container d-flex justify-content-between align-items-center">
+      <span> © 2023 Todos os direitos reservados </span>
+
+      <ul class="social">
+        <li>
+          <a id="linkedin"
+            href="https://www.linkedin.com/in/jos%C3%A9-antonio-conejo-madureira-785989230/"
+            class="text-hide"
+            >Linkedin</a
+          >
+        </li>
+        <li>
+          <a id="github" href="https://github.com/joseConejoMadureira" class="text-hide"
+            >Github</a
+          >
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+  <script>
+export default {};
+</script>
+  <style>
+.social a {
+  display: block;
+  width: 36px;
+  height: 36px;
+  text-indent: -9999px;
+}
+
+.social li {
+  display: inline-block;
+  margin-left: 1em;
+}
+#linkedin
+{
+  background: url(../assets/icone-linkedin.png);
+  background-size: 36px 36px;
+}
+
+#github
+{
+  background: url(../assets/icone-github.png);
+  background-size: 36px 36px;
+}
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: auto;
+  width: 100vw;
+  padding-top: 40px;
+}
+</style>
