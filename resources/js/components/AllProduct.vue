8b4e458f4eb9e52928c64a2bac0216d57ddc74f9
joseConejoMadureira <jose.games15@outlook.com>
2023-08-25 22:12:13 -0300

    Revert "datable"
    This reverts commit 132cbf5411e5141f441b6f1406b0be8108fb3772.

--- resources/js/components/AllProduct.vue
+++ resources/js/components/AllProduct.vue
@@ -1,62 +1,68 @@
 <template>
+  <div>
+    <h2 class="text-center">Products List</h2>
 
-    <div class="row">
-        <div class="col-lg-8 offset-lg-2">
-            <div class="table-responsive">
-                <DataTable :data="products" :columns="columns" class="table table-striped table-bordered display"
-                :options="{responsive:true,autoWidth:false, dom:'Bfrtip',language:{
-                    search:'Buscar', zeroRecords:'Não há registros',
-                    info: 'Mostrando inicio a _END_ de _TOTAL_ resgistros',
-                    infoFiltered: '(Filtrados de _MAX_ registros.)',
-                    paginate:{ first:'Primero',previous:'Anterior',next:'Seguinte',last:'Último'}
-                }}" >
-                    <thead>
-                        <tr><th>#</th><th>Name</th><th>Price</th></tr>
-                    </thead>
-                </DataTable>
-            </div>
-        </div>
+    <div class="table-responsive-sm">
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Action</th>
+            <!-- <th>Actions</th> -->
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="product in products" :key="product.id">
+            <th scope="row">{{ product.id }}</th>
+
+            <td>{{ product.name }}</td>
+            <td>{{ product.price }}</td>
+            <td>
+              <div class="btn-group" role="group">
+                <router-link
+                  :to="{ name: 'edit', params: { id: product.id } }"
+                  class="btn btn-success"
+                  >Edit</router-link
+                >
+                <button
+                  class="btn btn-danger"
+                  @click="deleteProduct(product.id)"
+                >
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
+
 <script>
-import axios from 'axios';
-import DataTable from 'datatables.net-vue3';
-import DataTableLib from 'datatables.net-bs5'
-
-import 'datatables.net-responsive-bs5';
-
-DataTable.use(DataTableLib);
-
-export default{
-    components: {DataTable},
-    data(){
-        return {
-            products:null,
-            columns:[
-                {data:null, render: function(data,type,row,meta)
-                {return `${meta.row+1}`}},
-                {data:'name'},
-                {data: null, render: function(data,type,row,meta)
-                {return ('$'+ new Intl.NumberFormat('es-mx').format(data.price))}}
-            ],
-
-        }
+import axios from "axios";
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:8000/api/products/")
+      .then((response) => (this.products = response.data));
+  },
+  methods: {
+    deleteProduct(id) {
+      axios
+        .delete(`http://localhost:8000/api/products/${id}`)
+        .then((response) => {
+          let i = this.products.map((data) => data.id).indexOf(id);
+          this.products.splice(i, 1);
+        });
     },
-    mounted(){
-        this.getProducts();
-    },
-    methods:{
-        getProducts(){
-            axios.get('http://localhost:8000/api/products/').then(
-                response => (
-                    this.products = response.data
-                )
-            );
-        }
-    }
-}
-
+  },
+};
 </script>
-<style>
-@import 'datatables.net-bs5';
-</style>
