8bb80f24dab63ea8d5845de9b9d0f069e734cce4
joseConejoMadureira <jose.games15@outlook.com>
2023-08-29 11:26:27 -0300

    products

--- resources/js/components/AllProduct.vue
+++ resources/js/components/AllProduct.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row justify-content-md-center">
-    <div class="col-md-auto">
+    <div class="col-md-6">
       <v-card>
         <v-card-title>
           Busca
@@ -18,7 +18,7 @@
           v-model:page="page"
           :search="search"
           :headers="headers"
-          :items="desserts"
+          :items="products"
           :items-per-page="itemsPerPage"
           hide-default-footer
           class="elevation-1"
@@ -52,186 +52,24 @@ export default {
       itemsPerPage: 5,
       search: "",
       headers: [
-        {
-          align: "start",
-          key: "name",
-          sortable: false,
-          title: "Dessert (100g serving)",
-        },
-        { title: "Calories", key: "calories" },
-        { title: "Fat (g)", key: "fat" },
-        { title: "Carbs (g)", key: "carbs" },
-        { title: "Protein (g)", key: "protein" },
-        { title: "Iron (%)", key: "iron" },
+
+        { title: "id", key: "id" },
+        { title: "name", key: "name" },
+        { title: "price", key: "price" },
         { title: "actions", key: "actions", sortable: false },
       ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: 1,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: 7,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: 8,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: 16,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: 0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: 2,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: 45,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: 22,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: 6,
-        },
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: 1,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: 7,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: 8,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: 16,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: 0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: 2,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: 45,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: 22,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: 6,
-        },
-      ],
+      products: []
+
     };
+  },
+  created() {
+    axios
+      .get("http://localhost:8000/api/products/")
+      .then((response) => (this.products = response.data));
   },
   computed: {
     pageCount() {
-      return Math.ceil(this.desserts.length / this.itemsPerPage);
+      return Math.ceil(this.products.length / this.itemsPerPage);
     },
   },
 };
