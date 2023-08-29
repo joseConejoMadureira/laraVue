bb08a0e0feaf2302b835268083f8d009d378cc46
joseConejoMadureira <jose.games15@outlook.com>
2023-08-29 12:52:55 -0300

    seeders

--- database/seeders/ProductsSeeder.php
+++ database/seeders/ProductsSeeder.php
@@ -24,6 +24,46 @@ class ProductsSeeder extends Seeder
             [
                 'name' => 'xbox 2',
                 'price' => 2500
+            ],
+            [
+                'name' => 'xbox 3',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 4',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 5',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 6',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 7',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 8',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 9',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 10',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 11',
+                'price' => 2500
+            ],
+            [
+                'name' => 'xbox 12',
+                'price' => 2500
             ]
         ]);
     }
