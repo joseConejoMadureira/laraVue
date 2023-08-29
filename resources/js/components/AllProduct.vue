f573212dd6098b5a2afa63869f0754df35532178
joseConejoMadureira <jose.games15@outlook.com>
2023-08-29 11:52:10 -0300

    edit

--- resources/js/components/AllProduct.vue
+++ resources/js/components/AllProduct.vue
@@ -28,9 +28,12 @@
               <v-pagination v-model="page" :length="pageCount"></v-pagination>
             </div>
           </template>
+
           <template v-slot:item.actions="{ item }">
-            <v-icon small class="mr-2" >mdi-pencil</v-icon>
-            <v-icon small >mdi-delete</v-icon>
+            <v-icon small class="mr-2" @click="edit(item.raw.id)"
+              >mdi-pencil</v-icon
+            >
+            <v-icon small>mdi-delete</v-icon>
           </template>
         </v-data-table>
       </v-card>
@@ -40,32 +43,35 @@
 <script>
 import { VDataTable } from "vuetify/labs/VDataTable";
 
-
 export default {
   components: {
-    VDataTable
+    VDataTable,
   },
   data() {
-
     return {
       page: 1,
       itemsPerPage: 5,
       search: "",
       headers: [
-
         { title: "id", key: "id" },
         { title: "name", key: "name" },
         { title: "price", key: "price" },
         { title: "actions", key: "actions", sortable: false },
       ],
-      products: []
-
+      products: [],
     };
   },
   created() {
     axios
       .get("http://localhost:8000/api/products/")
       .then((response) => (this.products = response.data));
+  },
+  methods: {
+    edit(id) {
+        console.log(JSON.stringify(id));
+
+      this.$router.push({ name: "edit", params: { id: id } });
+    },
   },
   computed: {
     pageCount() {
